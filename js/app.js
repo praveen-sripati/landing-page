29f729359fdeedefec25d174d6418a5efb9f913f
Praveen <pvnsripati@gmail.com>
2019-12-12 21:14:53 +0530

    feat: Add functionality to distinguish the section in view

--- js/app.js
+++ js/app.js
@@ -26,12 +26,14 @@
 */
 
 function isElementInViewport(el) {
+
   var rect = el.getBoundingClientRect();
+
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document. documentElement.clientWidth)
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
@@ -44,63 +46,82 @@ function isElementInViewport(el) {
 
 
 // build the nav
-const ulElement = document.getElementById('navbar__list');
+function navBuild() {
+  const ulElement = document.getElementById('navbar__list');
 
-const navLinkNames = ["Section 1", "Section 2", "Section 3", "Section 4"];
+  const navLinkNames = ["Section 1", "Section 2", "Section 3", "Section 4"];
 
-for (let i = 0; i <=3; i++) {
-  const liElement = document.createElement('li');
-  liElement.innerHTML = `<a href='#'>${navLinkNames[i]}</a>`;
-  liElement.classList.add('nav-link');
-  ulElement.appendChild(liElement);
+  for (let i = 0; i <=3; i++) {
+    const liElement = document.createElement('li');
+    liElement.innerHTML = `<a href='#'>${navLinkNames[i]}</a>`;
+    liElement.classList.add('nav-link');
+    ulElement.appendChild(liElement);
+  }
+
+  ulElement.style.cssText = `
+    display:flex;
+    flex=flow: row wrap;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem 0`;
+
+  const navElement = document.querySelectorAll('.navbar__menu');
+
+  navElement[0].setAttribute("style","background-color:rgba(0,13,60,1);");
+
+  const anchorElementsAll = ulElement.querySelectorAll('a');
+
+  for (let i = 0; i < anchorElementsAll.length; i++) {
+    anchorElementsAll[i].style.cssText = `padding:1rem;
+    text-decoration:none;
+    color:white;
+    `;
+    anchorElementsAll[i].setAttribute("onmouseover","hover(this)");
+    anchorElementsAll[i].setAttribute("onmouseout","normal(this)");
+  }
+
+  function hover(el) {
+    el.style.cssText =
+    `padding:1rem;
+    text-decoration:none;
+    color:black;
+    background-color: #fff`;
+  }
+
+  function normal(el) {
+    el.style.cssText =
+    `padding:1rem;
+    text-decoration:none;
+    color: white;
+    `;
+  }
+
+  // Add class 'active' to section when near top of viewport
+
+  function activeSection() {
+    const sections = document.querySelectorAll('section');
+
+    const anchors = document.querySelectorAll('a');
+
+    const handler = () => {
+      for (let i=0; i < sections.length; i++) {
+        if(isElementInViewport(sections[i])) {
+          sections[i].classList.add('active');
+          hover(anchors[i]);
+        } else {
+          sections[i].classList.remove('active');
+          normal(anchors[i]);
+        }
+      }
+    };
+
+    handler();
+    window.addEventListener('scroll', handler);
+  }
+  activeSection();
 }
-
-ulElement.style.cssText = `
-  display:flex;
-  flex=flow: row wrap;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem 0`;
-
-const navElement = document.querySelectorAll('.navbar__menu');
-
-navElement[0].setAttribute("style","background-color:rgba(0,13,60,1);");
-
-const anchorElementsAll = ulElement.querySelectorAll('a');
-
-for (let i = 0; i < anchorElementsAll.length; i++) {
-  anchorElementsAll[i].style.cssText = `padding:1rem;
-  text-decoration:none;
-  color:white;
-  `;
-  anchorElementsAll[i].setAttribute("onmouseover","hover(this)");
-  anchorElementsAll[i].setAttribute("onmouseout","normal(this)");
-}
-
-function hover(el) {
-  el.style.cssText =
-  `padding:1rem;
-  text-decoration:none;
-  color:black;
-  background-color: #fff`;
-}
-
-function normal(el) {
-  el.style.cssText =
-  `padding:1rem;
-  text-decoration:none;
-  color:white;
-  `;
-}
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
@@ -109,8 +130,10 @@ function normal(el) {
 */
 
 // Build menu
+document.addEventListener('DOMContentLoaded', navBuild);
 
 // Scroll to section on link click
+
 
 // Set sections as active
 
